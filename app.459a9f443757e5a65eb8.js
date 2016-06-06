6b18af3a042ae6eec0e2ca3bddcc0c2cb1c391e1
Riggs <jriggs@plch.net>
2016-06-06 10:54:49 -0400

    Updates

---
+++ app.459a9f443757e5a65eb8.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],[function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{"default":e}}var u=l(1),d=n(u),r=l(34),a=n(r);a["default"].render(d["default"].createElement("div",null,"Hello world!"),document.getElementById("app"))}]);
+//# sourceMappingURL=app.459a9f443757e5a65eb8.js.map
---
+++ style.963cfc8d5350ade32c08.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],{0:function(n,o,c){n.exports=c(164)},164:function(n,o){}});
+//# sourceMappingURL=style.963cfc8d5350ade32c08.js.map
